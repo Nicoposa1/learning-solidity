60a136c351faeac14888ad4160b250cb8e6d648b
Nicolas Posa <nicoposa57@gmail.com>
2021-10-21 17:10:21 -0300

    with draw money

--- SendMoneyExample.sol
+++ SendMoneyExample.sol
@@ -3,12 +3,24 @@
 pragma solidity ^0.8.1;
 
 contract sendMoneyExample {
+
+  uint public balanceRecived;
+
   function reciveMoney() public payable{
 
   }
 
-  function getBalnce() public view returns (uint) {
+  function getBalance() public view returns (uint) {
     return address(this).balance;
+  }
+
+  function withdrawMoney() public{
+    address payable to = payable(msg.sender);
+    to.transfer(this.getBalance());
+  }
+
+  function withdrawMoneyTo(address payable _to) public payable{
+    _to.transfer(this.getBalance());
   }
 
 }
